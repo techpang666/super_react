e0543c49e11f9736324831b26e0b0e66fc1bce51
沈公子 <techpang666@gmail.com>
2021-06-10 06:36:32 +0800

    use function component

--- src/JSXDemo.js
+++ src/JSXDemo.js
@@ -6,12 +6,18 @@ function sayHi() {
   )
 }
 
+// 函数式组件(少用 都是类组件)
+function clickHandler() {
+  console.log(666);
+}
+
 function JSXDemo() {
   return (
     <div>
       {666}
       { 1 < 2 ? 'true' : 'false' }
       {sayHi()}
+      <button onClick={clickHandler}>测试函数式组件</button>
     </div>
   )
 }
